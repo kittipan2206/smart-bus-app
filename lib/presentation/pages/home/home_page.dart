58dc72749d0c5ca55346bb37555b9d8e171c5964
kittipan2206 <s6330613017@phuket.psu.ac.th>
2023-11-27 12:56:27 +0700

    Add import statements for Flutter and Fluttertoast packages, and implement the logic to download and launch the Smart Bus apk. Update url_launcher package to version 6.1.14.

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:smart_bus/globals.dart';
 import 'package:smart_bus/map_page.dart';
 import 'package:smart_bus/presentation/pages/authen/login_page.dart';
@@ -10,6 +12,7 @@ import 'package:smart_bus/presentation/shared_controller/home/home_controller.da
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:smart_bus/presentation/pages/setting/setting.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -22,6 +25,57 @@ class HomePage extends StatelessWidget {
       child: SafeArea(
         child: Scaffold(
           appBar: AppBar(
+            leading: // download apk on web and android
+                (kIsWeb)
+                    ? CircularIconButton(
+                        icon: Icons.download_rounded,
+                        onPressed: () {
+                          Get.dialog(
+                            AlertDialog(
+                              title: const Text('Download Smart Bus'),
+                              content: const Text(
+                                  'Download Smart Bus apk to your device to use all features available on only android devices.'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Get.back();
+                                  },
+                                  child: const Text('Cancel'),
+                                ),
+                                TextButton(
+                                  onPressed: () async {
+                                    //open url
+                                    final uri = Uri.parse(
+                                        "https://drive.google.com/file/d/1YZhHHaYKrijJs9XGwlpRjpllfR_sUA9O/view?usp=sharing");
+                                    // if (await canLaunchUrl(uri)) {
+                                    try {
+                                      await launchUrl(uri);
+                                    } catch (e) {
+                                      Fluttertoast.showToast(
+                                          msg: "Can't launch url $e",
+                                          toastLength: Toast.LENGTH_SHORT,
+                                          gravity: ToastGravity.CENTER,
+                                          timeInSecForIosWeb: 1,
+                                          backgroundColor: Colors.red,
+                                          textColor: Colors.white,
+                                          fontSize: 16.0);
+                                    }
+
+                                    // } else {
+                                    //   // can't launch url
+                                    // }
+                                  },
+                                  child: const Text(
+                                    'Download',
+                                    style: TextStyle(color: Colors.blue),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      )
+                    : null,
             title: const Text('Smart Bus'),
             actions: [
               Obx(() => isLogin.value
